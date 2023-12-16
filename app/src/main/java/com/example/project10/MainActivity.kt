e2c60e68087af94d4a08834a91e68347874a89d4
jnjeri <jnjeri@iu.edu>
2023-12-15 21:27:38 -0500

    done

--- app/src/main/java/com/example/project10/MainActivity.kt
+++ app/src/main/java/com/example/project10/MainActivity.kt
@@ -12,12 +12,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.project10.ui.theme.Project10Theme
 
+/**
+ * the entry point of the app
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Project10Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -29,6 +31,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+/**
+ * function to display a greeting
+ *
+ * @param name The name to be included in the greeting
+ * @param modifier The modifier to be applied to the Text composable
+ */
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -37,6 +45,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+/**
+ * preview of the greeting function
+ */
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
